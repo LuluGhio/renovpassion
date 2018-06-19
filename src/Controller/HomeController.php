b07aeb0d8150749476c5bed5d1f2345fdb5d5ebc
Lulu Ghio <lucileghio@live.fr>
2018-06-19 19:56:04 +0000

    createFormBuilder

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Contact;
 
 class HomeController extends Controller
 {
@@ -24,5 +27,25 @@ class HomeController extends Controller
     
     public function home(){
         return $this->render('home/home.html.twig'); // render allows to call for the twig file
+    }
+    
+    
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function newContact(Request $request, ObjectManager $manager){
+        
+        $contact = new Contact();
+        // createFormBuilder() creates a form linked to $contact
+        $form = $this->createFormBuilder($contact)
+        // now let's configurate the form ! 
+                    ->add('prenom')
+                    ->add('nom')
+                    ->add('email')
+                    ->add('message')
+                    ->getForm();
+        
+        return $this->render('home/contact.html.twig');
+        
     }
 }
