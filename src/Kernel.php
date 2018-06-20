2ab1c3c5ad2d067e4dee06d5f7c85a9fdf72ed06
Lulu Ghio <lucileghio@live.fr>
2018-06-20 22:30:54 +0000

    EasyAdminBundle : installation done / chapI step2 in progress

--- src/Kernel.php
+++ src/Kernel.php
@@ -12,6 +12,7 @@ use Symfony\Component\Routing\RouteCollectionBuilder;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+    use App\EasyCorp\Bundle\EasyAdminBundle;
 
     const CONFIG_EXTS = '.{php,xml,yaml,yml}';
 
@@ -33,6 +34,11 @@ class Kernel extends BaseKernel
                 yield new $class();
             }
         }
+        
+        $bundles = array(
+            // ...
+            new EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle(),
+        );
     }
 
     protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader)
